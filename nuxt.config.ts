88c8362ee0f754b45d4ba5130e5ced239a16088f
tlaibacas <tiago.abreu.laibacas@gmail.com>
2025-04-04 15:33:43 +0100

    feat: add Leaflet integration in Nuxt config; update dependencies and create AddressMap component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  css: ["vuetify/styles"],
+  css: ["vuetify/styles", "leaflet/dist/leaflet.css"],
   build: {
     transpile: ["vuetify"],
   },
+  modules: ["@nuxtjs/leaflet"],
 });
